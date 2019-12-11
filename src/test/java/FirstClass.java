2e220684fb9f1d9e5facc7359b2ce78c07bf4d66
alihannamli <55026838+alihannamli@users.noreply.github.com>
2019-12-11 18:52:54 -0500

    removed one line

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -3,6 +3,5 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("alihan namli");
         System.out.println("hello world");
-        System.out.println("sdfasdfds");
     }
 }
