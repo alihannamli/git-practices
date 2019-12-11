788d60f106b7229fc60d6c1da0b98a3cc71ef5ca
Alihan Namli <namlialihan@gmail.com>
2019-12-11 18:49:53 -0500

    second commit

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,7 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("alihan namli");
+        System.out.println("hello world");
+        System.out.println("sdfasdfds");
     }
 }
---
+++ src/test/java/SecondClass.java
@@ -0,0 +1,8 @@
+public class SecondClass {
+
+    public static void main(String[] args) {
+
+        String myname = "Fatih";
+        System.out.println(myname);
+    }
+}
