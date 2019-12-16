dfb006b69916499624bc68cead277ead674ac1f5
Alihan Namli <namlialihan@gmail.com>
2019-12-15 23:53:14 -0500

    commit

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -3,5 +3,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("alihan namli");
         System.out.println("hello world");
+        System.out.println("hadsdadad");
+        System.out.println("asfasdasdgsld");
     }
 }
