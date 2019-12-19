6e11fec366775ee14db56a7f1fda85c380e904af
Alihan Namli <namlialihan@gmail.com>
2019-12-19 00:15:23 -0500

    branch

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -3,7 +3,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("alihan namli");
         System.out.println("hello world");
-        System.out.println("hadsdadad");
-        System.out.println("asfasdasdgsld");
+        System.out.println("this is from develop branch");
+        System.out.println("this is from develop branch");
+        System.out.println("this is from develop branch");
     }
 }
